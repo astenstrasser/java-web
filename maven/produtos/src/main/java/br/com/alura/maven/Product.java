621a2c56af244533adb82c9ed31a5308307dcd4f
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-06-18 18:26:46 +0200

    Added some code to try pmd

---
+++ maven/produtos/src/main/java/br/com/alura/maven/Product.java
@@ -0,0 +1,18 @@
+package br.com.alura.maven;
+
+public class Product {
+
+	private final String name;
+	private final double price;
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return this.name +" - "+ this.price;
+	}
+
+}
